129e84d9025051ba7a1b5dd99762137dc4f5b289
Carlos Velasquez <cv.velasquez@gmail.com>
2016-03-10 08:56:39 -0800

    my loop

--- js/app.js
+++ js/app.js
@@ -1,26 +1,34 @@
-var students = ['Sean', 'Jenny', 'Singh', 'Mark', 'Lisa'];
-for(var i = 0; i < students.length; i++) {
-  console.log("My name is " + students[i] + " and I'm going to be a developer.");
+//var students = ['Sean', 'Jenny', 'Singh', 'Mark', 'Lisa'];
+//for(var i = 0; i < students.length; i++) {
+//  console.log("My name is " + students[i] + " and I'm going to be a developer.");
+//}
+//
+//var index = 0;
+//var moreStudents = ['Ilya', 'David', 'Evan', 'Carlos', 'Ian'];
+//while(index < moreStudents.length) {
+//  console.log("My name is " + moreStudents[index] + " and I also go WDI in Santa Monica.");
+//  document.write("<p>My name is <span class='red'>" + moreStudents[index] + "</span> and I also go WDI in Santa Monica.</p>")
+//  index++;
+//}
+//
+//var students2 = ['Erik', 'Jenny', 'David', 'Carlos', 'Adam', 'Lisa'];
+//for (var i = 0; i < students2.length; i++ ) {
+//  console.log("My name is " + students2[i] + " and I'm going to be a developer.");
+//  document.write("<p>My name is <span class='red'>" + students2[i] + "</span> and I'm going to be a developer.</p>");
+//}
+//
+//var i = 0;
+//var staff = ['Phil', 'Matt', 'Kate', 'John', 'Philip'];
+//while (i < staff.length) {
+//  console.log("This wonderful person is teaching us everything we know about code: " + staff[i]);
+//  document.write("<p>his wonderful person is teaching us everything we know about code: <span class='red'>" + staff[i] + "</span></p>");
+//  i++;
+//}
+
+var classes = 0;
+var rooms = ["room1", "room2", "room3", "lounge"];
+while(classes < rooms.length ) {
+  console.log(rooms[classes]);
+  classes++;
 }
 
-var index = 0;
-var moreStudents = ['Ilya', 'David', 'Evan', 'Carlos', 'Ian'];
-while(index < moreStudents.length) {
-  console.log("My name is " + moreStudents[index] + " and I also go WDI in Santa Monica.");
-  document.write("<p>My name is <span class='red'>" + moreStudents[index] + "</span> and I also go WDI in Santa Monica.</p>")
-  index++;
-}
-
-var students2 = ['Erik', 'Jenny', 'David', 'Carlos', 'Adam', 'Lisa'];
-for (var i = 0; i < students2.length; i++ ) {
-  console.log("My name is " + students2[i] + " and I'm going to be a developer.");
-  document.write("<p>My name is <span class='red'>" + students2[i] + "</span> and I'm going to be a developer.</p>");
-}
-
-var i = 0;
-var staff = ['Phil', 'Matt', 'Kate', 'John', 'Philip'];
-while (i < staff.length) {
-  console.log("This wonderful person is teaching us everything we know about code: " + staff[i]);
-  document.write("<p>his wonderful person is teaching us everything we know about code: <span class='red'>" + staff[i] + "</span></p>");
-  i++;
-}
