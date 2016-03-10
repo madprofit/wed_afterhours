978475027bac60ba52cc0aa14437dbf083a6fa52
Jenny Kim <ms.jennykimmy@gmail.com>
2016-03-09 18:27:23 -0800

    added staff array

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 var students = ['Sean', 'Jenny', 'Singh', 'Mark', 'Lisa'];
 for(var i = 0; i < students.length; i++) {
-  console.log("My name is " + students[i] + " and I go WDI in Santa Monica.");
+  console.log("My name is " + students[i] + " and I'm going to be a developer.");
 }
 
 var moreStudents = ['Ilya', 'David', 'Evan', 'Carlos', 'Ian'];
 var index = 0;
 while(index < moreStudents.length) {
-  console.log("My name is " + moreStudents[index] + " and I go WDI in Santa Monica.");
+  console.log("My name is " + moreStudents[index] + " and I'm going to be a developer.");
   index++;
+}
+
+i = 0;
+
+var staff = ['Phil', 'Matt', 'Kate', 'John', 'Philip'];
+while (i < staff.length) {
+  console.log("This wonderful person is teaching us everything we know about code: " + staff[i]);
+  i++;
 }
