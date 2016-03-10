cf62c8eb37b736c02211355d636010f987638f5b
Erik Gomez <egoxiv@gmail.com>
2016-03-09 18:06:16 -0800

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var students = ['Sean', 'Jenny', 'Singh', 'Mark', 'Lisa'];
+for(var i = 0; i < students.length; i++) {
+  console.log("My name is " + students[i] + " and I go WDI in Santa Monica.");
+}
+
+var moreStudents = ['Ilya', 'David', 'Evan', 'Carlos', 'Ian'];
+var index = 0;
+while(index < moreStudents.length) {
+  console.log("My name is " + moreStudents[index] + " and I go WDI in Santa Monica.");
+  index++;
+}
